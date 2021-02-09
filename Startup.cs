e881c75a9a80541b6f57383ccaa309dfffe08183
SWAROOP Shivanna <swarooprooney@gmail.com>
2021-02-09 17:19:57 +0530

    Adding support to new CORS policy

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace JournalistTierAPI
           {
               options.AddDefaultPolicy(policy => policy.AllowAnyHeader()
                   .AllowAnyMethod().WithOrigins("https://localhost:4200"));
-              options.AddPolicy("prodpolicy", policy => policy.AllowAnyHeader()
+              options.AddDefaultPolicy(policy => policy.AllowAnyHeader()
                .AllowAnyMethod().WithOrigins("https://journalisttier.azurewebsites.net"));
           });
             services.AddIdentityServices(Configuration);
